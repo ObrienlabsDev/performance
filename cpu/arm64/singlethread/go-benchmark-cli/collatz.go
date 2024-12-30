d64f950cef3327898816d2bbffeb3e88f5c46b9a
Michael O'Brien <michael@obrienlabs.org>
2024-12-30 12:25:29 -0500

    #5 - collatz alpha 2

--- cpu/arm64/singlethread/go-benchmark-cli/collatz.go
+++ cpu/arm64/singlethread/go-benchmark-cli/collatz.go
@@ -9,19 +9,54 @@ import "fmt"
 func main() {
 	fmt.Println("Collatz 2024 michael@obrienlabs.dev")
 
-	var oddSearchStart uint64 = 1                  // must be odd
-	var oddSearchEnd uint64 = 18446744073709551615 // must be odd
+	var oddSearchStart uint64 = 1 // must be odd
+	var oddSearchEnd uint64 = 184 //4674 //4073709551615 // must be odd
+	var oddSearchIncrement uint64 = 2
+	var oddSearchCurrent uint64 = 1
 	var current uint64 = oddSearchStart
-	var path = 1
-	//var globalMaxValue uint64 = 1
-	//var globalMaxPath uint64 = 1
+	var path = 0
+	var globalMaxValue uint64 = 1
+	var globalMaxPath = 1
 	var maxValue uint64 = 1
-	//var maxPath uint64 = 1
 	var secondsStart = 1
 	var secondsCurrent = 1
 	var secondsLast = 1
 
-	for current = oddSearchStart; current < oddSearchEnd; current += 2 {
-		fmt.Println("mp:", current, "p:", path, "m:", maxValue, "sec:", (secondsCurrent - secondsLast), "dur:", secondsCurrent-secondsStart)
+	/**
+	  if odd divide by 2, if event multiply by 3 and add 1
+	*/
+	for oddSearchCurrent = oddSearchStart; oddSearchCurrent < oddSearchEnd; oddSearchCurrent += oddSearchIncrement {
+		current = oddSearchCurrent
+		path = 1
+		maxValue = 1
+
+		for {
+			if current%2 == 0 {
+				current = current >> 1
+			} else {
+				current = current<<1 + current + 1
+				// check limits
+				if current > maxValue {
+					maxValue = current
+				}
+			}
+
+			path++
+
+			// check completion of this number
+			if current < 2 {
+				// check limits
+				if maxValue > globalMaxValue {
+					globalMaxValue = maxValue
+					fmt.Println("m0:", oddSearchCurrent, "p:", path, "m:", maxValue, "sec:", (secondsCurrent - secondsLast), "dur:", secondsCurrent-secondsStart)
+				}
+				if path > globalMaxPath {
+					globalMaxPath = path
+					fmt.Println("mp:", oddSearchCurrent, "p:", path, "m:", maxValue, "sec:", (secondsCurrent - secondsLast), "dur:", secondsCurrent-secondsStart)
+				}
+				break
+			}
+		}
 	}
+	fmt.Println("completed: ", secondsCurrent-secondsStart)
 }
