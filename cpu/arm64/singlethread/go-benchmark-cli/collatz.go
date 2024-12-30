4ed8a9fdcee92e106408ee1e92ef46496b8ff36c
Michael O'Brien <michael@obrienlabs.org>
2024-12-30 13:31:42 -0500

    #5 - collatz draft go code

--- cpu/arm64/singlethread/go-benchmark-cli/collatz.go
+++ cpu/arm64/singlethread/go-benchmark-cli/collatz.go
@@ -4,13 +4,16 @@ package main
 Single threaded collatz sequence generator
 https://github.com/ObrienlabsDev/performance/issues/5
 */
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Collatz 2024 michael@obrienlabs.dev")
 
-	var oddSearchStart uint64 = 1 // must be odd
-	var oddSearchEnd uint64 = 184 //4674 //4073709551615 // must be odd
+	var oddSearchStart uint64 = 1          // must be odd
+	var oddSearchEnd uint64 = 184467440737 //09551615 // must be odd
 	var oddSearchIncrement uint64 = 2
 	var oddSearchCurrent uint64 = 1
 	var current uint64 = oddSearchStart
@@ -18,16 +21,15 @@ func main() {
 	var globalMaxValue uint64 = 1
 	var globalMaxPath = 1
 	var maxValue uint64 = 1
-	var secondsStart = 1
-	var secondsCurrent = 1
-	var secondsLast = 1
+	var secondsStart = time.Now()
+	var secondsLast = time.Now()
 
 	/**
 	  if odd divide by 2, if event multiply by 3 and add 1
 	*/
 	for oddSearchCurrent = oddSearchStart; oddSearchCurrent < oddSearchEnd; oddSearchCurrent += oddSearchIncrement {
 		current = oddSearchCurrent
-		path = 1
+		path = 0
 		maxValue = 1
 
 		for {
@@ -48,15 +50,17 @@ func main() {
 				// check limits
 				if maxValue > globalMaxValue {
 					globalMaxValue = maxValue
-					fmt.Println("m0:", oddSearchCurrent, "p:", path, "m:", maxValue, "sec:", (secondsCurrent - secondsLast), "dur:", secondsCurrent-secondsStart)
+					fmt.Println("m0:", oddSearchCurrent, "p:", path, "m:", maxValue, "ms:", time.Since(secondsLast).Milliseconds(), "dur:", time.Since(secondsStart).Seconds())
+					secondsLast = time.Now()
 				}
 				if path > globalMaxPath {
 					globalMaxPath = path
-					fmt.Println("mp:", oddSearchCurrent, "p:", path, "m:", maxValue, "sec:", (secondsCurrent - secondsLast), "dur:", secondsCurrent-secondsStart)
+					fmt.Println("mp:", oddSearchCurrent, "p:", path, "m:", maxValue, "ms:", time.Since(secondsLast).Milliseconds(), "dur:", time.Since(secondsStart).Seconds())
+					secondsLast = time.Now()
 				}
 				break
 			}
 		}
 	}
-	fmt.Println("completed: ", secondsCurrent-secondsStart)
+	fmt.Println("completed: ", time.Since(secondsStart))
 }
