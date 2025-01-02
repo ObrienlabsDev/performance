a3e1dadaa9648f724cabcff51e0d7f2bcfbccaba
Michael O'Brien <michael@obrienlabs.org>
2025-01-01 22:51:25 -0500

    #19 - adjust batch 10-12 20%

--- cpu/virtual/multithreaded/java-benchmark-cli/performance-nbi/src/main/java/dev/obrienlabs/performance/nbi/Collatz.java
+++ cpu/virtual/multithreaded/java-benchmark-cli/performance-nbi/src/main/java/dev/obrienlabs/performance/nbi/Collatz.java
@@ -70,7 +70,7 @@ public class Collatz {
 	}
 	
 	public void searchCollatzParallel(long oddSearchCurrent, long secondsStart) {
-		long batchBits = 10; // adjust this based on the chip architecture 
+		long batchBits = 12; // adjust this based on the chip architecture 
 		
 		long searchBits = 32;
 		long batches = 1 << batchBits;
