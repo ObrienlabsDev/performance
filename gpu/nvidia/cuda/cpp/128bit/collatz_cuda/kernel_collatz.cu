865cfcce11df3af5f2902ec2b0b1c39037a93f32
Michael OBrien <michael@obrienlabs.org>
2025-01-09 22:23:31 -0500

    #12 - test function for base collatz -clean

--- gpu/nvidia/cuda/cpp/128bit/collatz_cuda/kernel_collatz.cu
+++ gpu/nvidia/cuda/cpp/128bit/collatz_cuda/kernel_collatz.cu
@@ -484,31 +484,6 @@ void testCollatzCUDAKernel(/*unsigned long long* _input1, */unsigned long long _
         current0 = _input0;// [threadIndex] ;
         do {
             path += 1;
-            /*if (current0 % 2 == 0) {
-                current0 = current0 >> 1;
-                // shift high byte first
-                if (current1 % 2 != 0) {
-                    current0 += MAXBIT;
-                }
-                current1 = current1 >> 1;
-            }
-            else {
-                temp1 = 3ULL * current1;// + (current1 << 1);
-                current1 = temp1;
-
-                // shift first - calc overflow 1
-                temp0_sh = current0 << 1;
-                if (temp0_sh < current0) { // not yet
-                    current1 = current1 + 1ULL;
-                }
-                // add second - calc overflow 2
-                temp0_ad = temp0_sh + current0;
-                if (temp0_ad < temp0_sh) { // overflow - needs temp0sh
-                    current1 = current1 + 1ULL;
-                }
-                current0 = temp0_ad + 1ULL; // check overflow
-            */
-
             // keep copy of n
             temp0 = current0;
             temp1 = current1;
@@ -531,26 +506,7 @@ void testCollatzCUDAKernel(/*unsigned long long* _input1, */unsigned long long _
                 if (current0 < temp0) {
                     current1 += 2ULL;
                 }
-
-
-                /*temp1 = current1 + current1 + current1;// + (current1 << 1);
-                current1 = temp1;
-
-                // shift first - calc overflow 1
-                temp0_sh = current0 + current0;// 1ULL + (current0 << 1);
-                if (temp0_sh < current0) {
-                    current1 = current1 + 1ULL; // check overflow
-                }
-                // add second - calc overflow 2
-                temp0_ad = temp0_sh + current0;
-                if (temp0_ad < temp0_sh) { // overflow
-                    current1 = current1 + 1ULL; // check overflow
-                }*/
-                // finally add 2 (if we combine odd/even) - otherwise 2
                 current0 += 1ULL; // check overflow
-
-
-
 
                 // check for max (if combined odd/even mult by 2)
                 if (max1 < current1) {
