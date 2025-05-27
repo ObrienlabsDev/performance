ba98729341eeb87eac5df3aed9d1d86291f30faa
Michael OBrien <michael@obrienlabs.org>
2025-05-27 09:19:49 -0400

    #24 - adjust batch

--- cpu/virtual/multithreaded/128bit/java-benchmark-128-cli/performance-nbi/batch.sh
+++ cpu/virtual/multithreaded/128bit/java-benchmark-128-cli/performance-nbi/batch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-LAST1=36
+LAST1=37
 echo $LAST1
-for i in {15..24}; do
+for i in {15..26}; do
   echo "running 0 $LAST1 $i"
   java -cp target/performance-nbi-0.0.1-SNAPSHOT.jar dev.obrienlabs.performance.nbi.Collatz128bit 0 $LAST1 $i
 done
