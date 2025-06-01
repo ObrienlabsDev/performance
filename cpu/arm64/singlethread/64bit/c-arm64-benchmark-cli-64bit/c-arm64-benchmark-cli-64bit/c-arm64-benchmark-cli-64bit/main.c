6bfa184899aed23f4b74f46c02483515b0660102
Michael O'Brien <michael@obrienlabs.org>
2025-05-31 22:26:53 -0400

    #41 - apple silicon c

---
+++ cpu/arm64/singlethread/64bit/c-arm64-benchmark-cli-64bit/c-arm64-benchmark-cli-64bit/c-arm64-benchmark-cli-64bit/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c-arm64-benchmark-cli-64bit
+//
+//  Created by Michael O'Brien on 2025/5/31.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
