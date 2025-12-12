2495d66629952e867aa7866c7d76ab41d80ffa4b
Michael O'Brien <michael@obrienlabs.org>
2025-12-12 18:45:14 -0500

    #42 - start with vector species

--- cpu/virtual/vector/java-vector-api-cli/src/main/java/dev/obrienlabs/vector/java_vector_api_cli/VectorCli.java
+++ cpu/virtual/vector/java-vector-api-cli/src/main/java/dev/obrienlabs/vector/java_vector_api_cli/VectorCli.java
@@ -1,12 +1,19 @@
 package dev.obrienlabs.vector.java_vector_api_cli;
 
+import jdk.incubator.vector.*;
+
 /**
  * https://github.com/ObrienlabsDev/performance/issues/42
  */
-public class VectorCli
-{
-    public static void main( String[] args )
-    {
+public class VectorCli {
+	
+	static final VectorSpecies<Float> SPECIES = FloatVector.SPECIES_PREFERRED;
+	
+    public static void main( String[] args) {
         VectorCli vectorCli = new VectorCli();
+        int N = 1024; 
+        float[][] A = new float[N][N];
+        float[][] B = new float[N][N];
+        float[][] C = new float[N][N];
     }
 }
