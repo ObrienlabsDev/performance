cc1e768dcd42214e74cdc2b9196bdad8fec4065e
Michael OBrien <michael@obrienlabs.org>
2025-12-11 22:19:25 -0500

    #42 - Java Vector API stub

---
+++ cpu/virtual/vector/java-vector-api-cli/src/main/java/dev/obrienlabs/vector/java_vector_api_cli/VectorCli.java
@@ -0,0 +1,10 @@
+package dev.obrienlabs.vector.java_vector_api_cli;
+
+
+public class VectorCli
+{
+    public static void main( String[] args )
+    {
+        VectorCli vectorCli = new VectorCli();
+    }
+}
